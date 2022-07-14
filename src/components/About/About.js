e6d04aab040627abc0e7211a4c942df2e4d757a7
Eric Einerson <eaeiner.gmail.co>
2022-07-14 11:06:17 -0600

    added button hiding to About section for interactivity

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./about.css"
 import ME from "../../Assets/self.jpeg"
 import {BsPerson} from "react-icons/bs"
@@ -8,41 +8,55 @@ import {VscFolderLibrary} from "react-icons/vsc"
 
 
 function About() {
+
+  const [showExperience, setShowExperience] = useState(false);
+  const [showOther, setShowOther] = useState(false);
+  const [showProject, setShowProject] = useState(false);
+
+
   return (
     <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
       <div className="container about__container">
-        <div className='about__me'>
+        <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="About Image"/>
+            <img src={ME} alt="About Image" />
           </div>
         </div>
 
         <div className="about__content">
-
           <div className="about__cards">
-            <article className="about__card">
-              <BsPerson className="about__icon"/>
+            <article
+              className="about__card"
+              onClick={() => setShowExperience(!showExperience)}
+            >
+              <BsPerson className="about__icon" />
               <h5>Development Experience</h5>
-              <small>Flatiron School Graduate</small>
+              {showExperience && <small>Flatiron School Graduate</small>}
             </article>
 
-            <article className="about__card">
-              <FiUsers className="about__icon"/>
+            <article
+              className="about__card"
+              onClick={() => setShowOther(!showOther)}
+            >
+              <FiUsers className="about__icon" />
               <h5>Other Experience</h5>
-              <small>Management, Education, Healthcare</small>
+              {showOther && <small>Management, Education, Healthcare</small>}
             </article>
 
-            <article className="about__card">
-              <VscFolderLibrary className="about__icon"/>
+            <article
+              className="about__card" onClick={() => setShowProject(!showProject)}>
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>Project Count: 4</small>
+              {showProject && <small>Project Count: 4</small>}
             </article>
           </div>
 
-          <a href="contact" className="btn btn-primary">Let's Talk</a>
+          <a href="contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
