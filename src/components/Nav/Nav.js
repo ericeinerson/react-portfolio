9f94bf4e4499ab7bf60920d6868190f3c6e86089
Eric Einerson <eaeiner.gmail.co>
2022-08-04 13:15:06 -0600

    added tooltips to nav using React-tooltip

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,79 @@
-import React, {useState} from 'react'
-import "./nav.css"
-import {AiOutlineHome} from "react-icons/ai"
-import {FcAbout} from "react-icons/fc"
-import {BiBook} from "react-icons/bi"
-import {RiServiceLine} from "react-icons/ri"
-import {BiMessageSquareDetail} from "react-icons/bi"
+import React, {useState} from 'react';
+import "./nav.css";
+import {AiOutlineHome} from "react-icons/ai";
+import {FcAbout} from "react-icons/fc";
+import {BiBook} from "react-icons/bi";
+import {RiServiceLine} from "react-icons/ri";
+import {BiMessageSquareDetail} from "react-icons/bi";
+import ReactTooltip from "react-tooltip";
 
 function Nav() {
 
   const [activeNav, setActiveNav] = useState("#")
+  const [tooltip, setTooltip] = useState(true);
 
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><AiOutlineHome /></a>
-      <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><FcAbout /></a>
-      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBook /></a>
-      <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><RiServiceLine /></a>
-      <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><BiMessageSquareDetail /></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+        data-tip
+        data-for="tooltip-top"
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+        data-tip
+        data-for="tooltip-about"
+      >
+        <FcAbout />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}
+        data-tip
+        data-for="tooltip-experience"
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav('#services')}
+        className={activeNav === '#services' ? 'active' : ''}
+        data-tip
+        data-for="tooltip-services"
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+        data-tip
+        data-for="tooltip-contact"
+      >
+        <BiMessageSquareDetail />
+      </a>
+      <ReactTooltip id="tooltip-top" place="top" effect="solid">
+        Top
+      </ReactTooltip>
+      <ReactTooltip id="tooltip-about" place="top" effect="solid">
+        About
+      </ReactTooltip>
+      <ReactTooltip id="tooltip-experience" place="top" effect="solid">
+        Experience
+      </ReactTooltip>
+      <ReactTooltip id="tooltip-services" place="top" effect="solid">
+        Services
+      </ReactTooltip>
+      <ReactTooltip id="tooltip-contact" place="top" effect="solid">
+        Contact
+      </ReactTooltip>
     </nav>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 // import App from "./App"
 // import "./index.css"
 
-
-// ReactDOM.render(<App />,document.querySelector("#root"))
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -12,8 +9,6 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
